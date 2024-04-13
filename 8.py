a96ed41139b027e8ecf64531e2758884425ced02
Darialr <darial.ryaguzova@gmail.com>
2024-04-13 13:12:21 +0700

    ready

--- 8.py
+++ 8.py
@@ -1,5 +1,11 @@
-num = input('Введите число->')
-while str.isdigit(num) != 1:
-    print('Ошибка. Попробуйте еще раз. ', end="")
-    num = input('Введите число->')
-print(f'Введено целое число: {num}')
+s = '123456789'
+for i in s:
+    print(f'{s[:int(i)]} * 8 + {i} = {int(s[:int(i)]) * 8 + int(i)}')
+print()
+
+for i in s:
+    print(f'{s[:int(i)]} * 9 + {i} = {int(s[:int(i)]) * 9 + (int(i) + 1)}')
+print()
+
+for i in range(1, 10):
+    print(f'{"1" * i} * {"1" * i} = {int("1" * i) * int("1" * i)} ')
