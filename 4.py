5a60cd6903b58eaed037ed4fa2e7ebd00a3b5049
Darialr <darial.ryaguzova@gmail.com>
2024-04-13 12:49:52 +0700

    ready

--- 4.py
+++ 4.py
@@ -1,5 +1,6 @@
 x = int(input('Введите целое число->'))
-summ = 0
-for i in range(1, x + 1):
-    summ += i
-print(summ)
+for j in range(1, x + 1):
+    summ = 0
+    for i in range(1, j + 1):
+        summ += i
+    print(summ)
