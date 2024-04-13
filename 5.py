7b73099017efb10b502ae1e93cf5e3b9a266a652
Darialr <darial.ryaguzova@gmail.com>
2024-04-13 12:28:25 +0700

    ready

--- 5.py
+++ 5.py
@@ -1,1 +1,14 @@
-n = int(input('Введите целое число->'))
+def perfect_number(number):
+    sum_divisors = 0
+    for i in range(1, number):
+        if number % i == 0:
+            sum_divisors += i
+    return sum_divisors == number
+
+
+k = 0
+num = int(input("Введите число: "))
+for i in range(2, num):
+    if perfect_number(i) == 1:
+        k += 1
+print(k)
